6c3a6d21fa28525280ebfa314c4953978615cee5
Brackly Murunga <khanbrackly@gmail.com>
2023-04-11 18:51:28 +0000

    changed url

--- src/hooks/useRefresh.js
+++ src/hooks/useRefresh.js
@@ -5,7 +5,7 @@ const useRefreshToken = () => {
   const { dispatch } = useAuth();
 
   const refresh = async () => {
-    const response = await axios.get("https://equity-backend.blackmoss-55678a4d.westus2.azurecontainerapps.io/refresh", {
+    const response = await axios.get("https://equity-backend.bravewave-974a5699.westus2.azurecontainerapps.io/refresh", {
       withCredentials: true,
     });
     // const accessToken = response?.data?.accessToken;
