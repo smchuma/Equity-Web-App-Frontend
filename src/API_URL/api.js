a3436bee7d5bf1eea4817a6377eb44d5637617a0
Brackly Murunga <khanbrackly@gmail.com>
2023-04-16 17:54:55 +0300

    Update api.js

--- src/API_URL/api.js
+++ src/API_URL/api.js
@@ -2,4 +2,4 @@ export const BASEURL =
   "https://equity-backend.bravewave-974a5699.westus2.azurecontainerapps.io";
 
 export const ENSERSURL =
-  "http://ensers-for-equity.eastus.cloudapp.azure.com:5000/ensers";
+  "https://ensers.fusure.africa/ensers";
